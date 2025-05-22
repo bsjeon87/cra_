ef780b4f566df8c4816a68964912eaf44afb4344
Bongsu Jeon <bongsu.jeon@samsung.com>
2025-05-22 13:40:29 +0900

    Update

--- CRA/main.cpp
+++ CRA/main.cpp
@@ -7,7 +7,18 @@
 
 using namespace std;
 
-void input(string str) {
+void test_input_using_mission1() {
+	ifstream fin{ "keyword_weekday_500.txt" }; //500개 데이터 입력
+
+	for (int i = 0; i < 500; i++) {
+		string week, word;
+		fin >> word >> week;
+		string ret = corrector_fun(word, week); // for mission 2
+		std::cout << ret << "\n";
+	}
+}
+
+void test_input_using_mission2(string str) {
 	ifstream fin{ "keyword_weekday_500.txt" }; //500개 데이터 입력
 
 	SimilarAlgorithmFactory similarAlgorithmFactory;
@@ -18,18 +29,20 @@ void input(string str) {
 	for (int i = 0; i < 500; i++) {
 		string week, word;
 		fin >> word >> week;
-		//string ret = corrector_fun(word,week); // for mission 1
 		string ret = corrector.processCorrect(word, week); // for mission 2
 		std::cout << ret << "\n";
 	}
 }
-
+#define MISSION_2
 int main(int argc, char* argv[]) {
+#if defined(MISSION_1)
+	test_input_using_mission1();
+#elif defined (MISSION_2)
 	string similarAlgorithm = "LevenshteinAlgorithm";
 	if (argc == 2) {
 		similarAlgorithm = argv[1];
 	}
-	input(similarAlgorithm);
-
+	test_input_using_mission2(similarAlgorithm);
+#endif
 	return 0;
 }
--- CRA/mission2/LevenshteinAlgorithm.cpp
+++ CRA/mission2/LevenshteinAlgorithm.cpp
@@ -3,16 +3,16 @@
 
 // 레벤슈타인 거리 계산 알고리즘 (문자열 유사도 검사)
 int LevenshteinAlgorithm::getSimilar(const std::string& a, const std::string& b) {
-	const size_t len_a = a.size();
-	const size_t len_b = b.size();
+	const size_t len_a = (int)a.size();
+	const size_t len_b = (int)b.size();
 
 	std::vector<std::vector<int>> d(len_a + 1, std::vector<int>(len_b + 1));
 
-	for (size_t i = 0; i <= len_a; ++i) d[i][0] = i;
-	for (size_t j = 0; j <= len_b; ++j) d[0][j] = j;
+	for (int i = 0; i <= len_a; ++i) d[i][0] = i;
+	for (int j = 0; j <= len_b; ++j) d[0][j] = j;
 
-	for (size_t i = 1; i <= len_a; ++i) {
-		for (size_t j = 1; j <= len_b; ++j) {
+	for (int i = 1; i <= len_a; ++i) {
+		for (int j = 1; j <= len_b; ++j) {
 			if (a[i - 1] == b[j - 1])
 				d[i][j] = d[i - 1][j - 1];
 			else
